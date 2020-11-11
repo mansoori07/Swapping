d5dfd206e266f7e9a44abf8b0a489c16696ef77f
mansoori07 <74285749+mansoori07@users.noreply.github.com>
2020-11-11 12:35:28 +0530

    Update Swap.c

--- Swap.c
+++ Swap.c
@@ -1,12 +1,13 @@
-#include <studio.h>
-main()
+#include <stdio.h>
+ main()
 {
-printf("Enter two no.");
-int a=0,b=0;
-scanf("%d",&a);
-scanf("%d",&b);
-a=a+b;
-b=a-b;
-a=a-b;
-printf("Swapping numbers are:"+a" & "+b);
+	printf("Enter two no.");
+	int a=0,b=0;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	printf("Swap words are: %d",a);
+	printf(" and %d",b);
 }
